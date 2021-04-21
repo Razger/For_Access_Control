335a1423bedcd9e01c61ef354233f0138257ec6d
Mikhail Burdastikov <mikhail.burdastikov@esedulainen.fi>
2021-04-21 14:37:15 +0300

    21.04.2021

--- PythonTest.py
+++ PythonTest.py
@@ -3,27 +3,61 @@ conn = pyodbc.connect("""
 Driver={ODBC Driver 17 for SQL Server};
 Server=tcp:DESKTOP-RPQNDU8,49172;
 Database=For_Access_Control;
-UID=Black;
-PWD=1156383;
+UID=Guest;
+PWD=Guest;
 """)
 
 con = 'y'
+ID_REG = 0
+LOGIN = 0
 loginned = False
 while ((con == 'y') and (not loginned)):
 
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM dbo.Reg')
+    #cursor = conn.cursor()
+    #cursor.execute('SELECT * FROM dbo.Reg')
 
-    print("Input login", end = ' ')
-    str_l = input()
-    print("Input password", end = ' ')
-    str_p = input()
+    #print("Input login", end = ' ')
+    #str_l = input()
+    #print("Input password", end = ' ')
+    #str_p = input()
+    
+    #for row in cursor:
+    #    if((str_l == row.Login) and (str_p == row.Password)):
+    #        print("IN login")
+    #        loginned = True
+    #        break
+    #if not loginned:
+    #    print("Try again (y/n)?")
+    #    con = input()
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM dbo.NFC_Card')
+
+    print("Attach your card")
+    ID_CARD = int(input())
 
     for row in cursor:
-        if((str_l == row.Login) and (str_p == row.Password)):
-            print("IN login")
+        #print(row.Id_Card, end = ' = ')
+        #print(ID_CARD)
+        if ID_CARD == row.Id_Card:
+            ID_REG = row.Id_Reg
+            cursor.commit()
+            #cursor = conn.cursor()
+            cursor.execute('SELECT * FROM dbo.Reg')
+
+
+            for row in cursor:
+                if ID_REG == row.Id_Reg:
+                    LOGIN = row.Login
+            cursor.commit()
+
+            print("Entry allowed ",LOGIN)
             loginned = True
             break
+
+
     if not loginned:
         print("Try again (y/n)?")
         con = input()
+conn.close()
+
